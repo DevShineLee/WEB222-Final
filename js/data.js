59096f3dc01997d5271e8c9ae527d62b7c579feb
vanthalos <vannthalos@gmail.com>
2023-08-06 16:26:59 -0400

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,20 @@
+window.reviewData = [
+    {
+        name: 'Shine',
+        date: '2023-08-03',
+        rating: 5,
+        reviewText: 'I`ve been playing this game about 5 years! So cool!',
+    },
+    {
+        name: 'Player1',
+        date: '2023-08-01',
+        rating: 5,
+        reviewText: 'One of the best MMORPGs I have ever played. The world is massive and full of adventures.',
+      },
+      {
+        name: 'Player2',
+        date: '2023-08-02',
+        rating: 4,
+        reviewText: 'I love the rich lore and the variety of classes to choose from. The raids are challenging and rewarding.',
+      }
+  ];
